dad7b71510ad704c2c27f8d8d83bc076b0075c20
samyinz <yinz84@gmail.com>
2017-02-21 17:25:32 +0800

    facebook authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ApplicationRecord
+
   include Clearance::User
 
   has_many :authentications, :dependent => :destroy
 
   def self.create_with_auth_and_hash(authentication, auth_hash)
-      user = User.create!(name: auth_hash["name"], email: auth_hash["extra"]["raw_info"]["email"])
+      user = User.create!(name: auth_hash["info"]["name"], email: auth_hash["extra"]["raw_info"]["email"])
       user.authentications << (authentication)
       return user
   end
@@ -12,6 +13,10 @@ class User < ApplicationRecord
   def fb_token
     x = self.authentications.where(:provider => :facebook).first
     return x.token unless x.nil?
+  end
+
+  def password_optional?
+    true
   end
 
 end
